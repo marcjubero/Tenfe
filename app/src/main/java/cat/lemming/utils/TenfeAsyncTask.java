36e23b0bd102dde4817a36d554b8108078573e43
marc <wrrzag@gmail.com>
2015-06-29 21:41:51 +0200

    Splash + first async

---
+++ app/src/main/java/cat/lemming/utils/TenfeAsyncTask.java
@@ -0,0 +1,26 @@
+package cat.lemming.utils;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by Marc on 29/06/2015.
+ */
+public class TenfeAsyncTask extends AsyncTask {
+
+    private static String BASE_URL = "http://tenfe.lemming.cat/API/";
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected Object doInBackground(Object[] params) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Object o) {
+        super.onPostExecute(o);
+    }
+}
